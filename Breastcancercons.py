7bc57582776c230abe25d909ec39cc99e0279c26
Rohit Midha <rohit.midha23@gmail.com>
2018-12-28 22:17:04 +0530

    Added : lots of stuff

--- Breastcancercons.py
+++ Breastcancercons.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 from sklearn import tree
-from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
@@ -25,7 +25,7 @@ del dataset['Unnamed: 32']
 del dataset['id']
 
 
-X = dataset.values[:,1:] 
+X = dataset.values[:,1:]
 y = dataset.values[:,0]
 u=[]
 
@@ -33,97 +33,97 @@ print ("Dataset Length: ", len(dataset))
 print ("Dataset Shape:: ", dataset.shape)
 
 for i in y:
-	if(i=='M'):	
+	if(i=='M'):
 		u.append(1)
-	else:	
+	else:
 		u.append(0)
 y=np.array(u)
 print(np.shape(y))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=23) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=23)
 
 model = GaussianNB()
 model.fit(X_train, y_train)
-y_prednb = model.predict(X_test) 
-print(confusion_matrix(y_test,y_prednb))  
+y_prednb = model.predict(X_test)
+print(confusion_matrix(y_test,y_prednb))
 #print(classification_report(y_test, y_pred))
-print("Naive Bayes ",accuracy_score(y_test,y_prednb)) 
+print("Naive Bayes ",accuracy_score(y_test,y_prednb))
 svc = SVC(kernel = 'linear',C=1, gamma=10, probability = True)
 y_predsvm = svc.fit(X_train, y_train).predict(X_test)
-print(confusion_matrix(y_test,y_predsvm ))  
+print(confusion_matrix(y_test,y_predsvm ))
 #print(classification_report(y_test, y_pred))
 print("SVC ",accuracy_score(y_test,y_predsvm ))
 
 
 clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(X_train, y_train)
 y_predlr = clf.fit(X_train, y_train).predict(X_test)
-print(confusion_matrix(y_test,y_predlr))  
+print(confusion_matrix(y_test,y_predlr))
 #print(classification_report(y_test, y_pred))
 print("Logistic regression ",accuracy_score(y_test,y_predlr))
 
 
-cols = ['radius_worst', 
-        'texture_worst', 
-        'perimeter_worst', 
-        'area_worst', 
-        'smoothness_worst', 
-        'compactness_worst', 
+cols = ['radius_worst',
+        'texture_worst',
+        'perimeter_worst',
+        'area_worst',
+        'smoothness_worst',
+        'compactness_worst',
         'concavity_worst',
-        'concave points_worst', 
-        'symmetry_worst', 
+        'concave points_worst',
+        'symmetry_worst',
         'fractal_dimension_worst']
 dataset=dataset.drop(cols, axis=1)
 
 cols = ['perimeter_mean',
-        'perimeter_se', 
-        'area_mean', 
+        'perimeter_se',
+        'area_mean',
         'area_se']
 dataset=dataset.drop(cols, axis=1)
 
 cols = ['concavity_mean',
-        'concavity_se', 
-        'concave points_mean', 
+        'concavity_se',
+        'concave points_mean',
         'concave points_se']
 dataset=dataset.drop(cols, axis=1)
 X = dataset.drop('diagnosis',axis=1)
 y = dataset['diagnosis']
 u2=[]
 for i in y:
-	if(i=='M'):	
+	if(i=='M'):
 		u2.append(1)
-	else:	
+	else:
 		u2.append(0)
 y=np.array(u2)
 print(np.shape(y))
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=23)  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=23)
 rf=RandomForestClassifier(random_state=23,n_estimators = 200, max_features=6,criterion = 'entropy')
 #rf=RandomForestClassifier(n_estimators=200,max_features=6,criterion="entropy")
 rf.fit(X_train,y_train)
-y_predrfe= rf.predict(X_test)  
-print(confusion_matrix(y_test,y_predrfe))  
+y_predrfe= rf.predict(X_test)
+print(confusion_matrix(y_test,y_predrfe))
 #print(classification_report(y_test, y_pred2))
 print("Random forest entropy ",accuracy_score(y_test,y_predrfe))
 
 rf=RandomForestClassifier(random_state=23,n_estimators=200,max_features=6,criterion="gini")
 rf.fit(X_train,y_train)
-y_predrfg = rf.predict(X_test)  
-print(confusion_matrix(y_test, y_predrfg))  
+y_predrfg = rf.predict(X_test)
+print(confusion_matrix(y_test, y_predrfg))
 #print(classification_report(y_test, y_pred))
 print("Random forest gini ",accuracy_score(y_test,y_predrfg))
 
 rf=ExtraTreesClassifier(random_state=23,n_estimators=200,max_features=6,criterion="entropy")
 rf.fit(X_train,y_train)
-y_predete = rf.predict(X_test)  
-print(confusion_matrix(y_test,y_predete))  
+y_predete = rf.predict(X_test)
+print(confusion_matrix(y_test,y_predete))
 #print(classification_report(y_test, y_pred))
 print("Extra tree entropy ",accuracy_score(y_test,y_predete))
 
 rf=ExtraTreesClassifier(random_state=23,n_estimators=200,max_features=6,criterion="gini")
 rf.fit(X_train,y_train)
-y_predetg = rf.predict(X_test)  
-print(confusion_matrix(y_test,y_predetg))  
+y_predetg = rf.predict(X_test)
+print(confusion_matrix(y_test,y_predetg))
 #print(classification_report(y_test, y_pred))
 print("Extra tree gini ",accuracy_score(y_test,y_predetg))
 
@@ -161,9 +161,9 @@ y_pred = model.predict(X_test)
 y_pred = (y_pred > 0.5)
 y_predann = []
 for i in range(len(y_pred)):
-        if y[i] == True: 
+        if y[i] == True:
                 y_predann.append(1)
-        else: 
+        else:
                 y_predann.append(0)
 
 print(confusion_matrix(y_te, y_pred))
@@ -176,6 +176,6 @@ for i in range(len(yoverall)):
 	else:
 		yoverall[i]=0
 
-print(confusion_matrix(y_test, yoverall))  
+print(confusion_matrix(y_test, yoverall))
 print(classification_report(y_test, yoverall))
 print("overall ",accuracy_score(y_test, yoverall))
--- bc_ann.py
+++ bc_ann.py
@@ -5,11 +5,23 @@ import seaborn as sns
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import confusion_matrix,classification_report,roc_auc_score, roc_curve, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from keras.layers import Flatten
+
+def plot_roc(y_test, y_pred, model_name):
+    fpr, tpr, thr = roc_curve(y_test, y_pred)
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.plot(fpr, tpr, 'k-')
+    ax.plot([0, 1], [0, 1], 'k--', linewidth=.5)  # roc curve for random model
+    ax.grid(True)
+    ax.set(title='ROC Curve for {} '.format(model_name),
+           xlim=[-0.01, 1.01], ylim=[-0.01, 1.01])
+    p = ax.get_figure()
+    p.savefig('annplot.jpg')
+
 
 df = pd.read_csv('/Users/rohit/BreastCancer/breast_cancer.csv')
 df = df.drop('Unnamed: 32', axis=1)
@@ -35,11 +47,13 @@ model.add(Dense(16, init='uniform', activation='relu'))
 model.add(Dropout(p=0.1))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 model.compile(optimizer='RMSProp', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, batch_size=100, nb_epoch=512)
+model.fit(X_train, y_train, batch_size=100, nb_epoch=52)
 
 y_pred = model.predict(X_test)
+y_pred_proba = model.predict_proba(X_test)
 y_pred = (y_pred > 0.5)
 
 print(confusion_matrix(y_test, y_pred))
 
 print(classification_report(y_test,y_pred))
+plot_roc(y_test, y_pred_proba,'ANN')
